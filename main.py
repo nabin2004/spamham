8c76630dfa139a436b0d59303eba709bbee05117
nabin2004 <olinabin2004@gmail.com>
2023-09-21 17:50:37 +0545

    added

---
+++ main.py
@@ -0,0 +1,33 @@
+import joblib
+import pandas as pd
+from flask import Flask, request, jsonify
+from sklearn.feature_extraction.text import CountVectorizer
+
+app = Flask(__name__)
+
+# Load the trained Multinomial NB model
+model = joblib.load('multinomial_nb_model.joblib')
+
+# Load the CountVectorizer used during training
+# Note: You need to have access to the vectorizer used during training
+# to transform new email data in the same way.
+vectorizer = joblib.load('count_vectorizer.joblib')
+
+@app.route('/classify_email', methods=['POST'])
+def classify_email():
+    data = request.get_json()
+    email_content = data['email_content']
+    
+    # Preprocess the email content and transform using the CountVectorizer
+    email_count = vectorizer.transform([email_content])
+    
+    # Predict using the model
+    prediction = model.predict(email_count)
+    
+    # Assume 1 indicates spam and 0 indicates ham
+    result = 'spam' if prediction[0] == 1 else 'ham'
+    
+    return jsonify({'prediction': result})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
